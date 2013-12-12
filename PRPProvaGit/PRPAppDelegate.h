347f596ca8dd69928bc1b318f330e0322148be91
onlyzanna <onlyzanna@hotmail.it>
2013-12-12 17:27:51 +0100

    Initial Commit

---
+++ PRPProvaGit/PRPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRPAppDelegate.h
+//  PRPProvaGit
+//
+//  Created by Davide Zanin on 12/12/13.
+//  Copyright (c) 2013 Davide Zanin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PRPProvaGit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PRPProvaGit
+//
+//  Created by Davide Zanin on 12/12/13.
+//  Copyright (c) 2013 Davide Zanin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PRPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PRPAppDelegate class]));
+    }
+}
